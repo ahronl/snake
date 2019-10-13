b150bac459daafb3e459f8847a4fe3ea7d3a537f
ahronl <aharon.levi@amobee.com>
2019-10-13 11:44:07 +0300

    remove redunded deps

--- project.clj
+++ project.clj
@@ -11,7 +11,4 @@
                  [org.clojure/test.generative "0.1.2"]
                  [ring "0.3.11" :exclusions [org.clojure/clojure
                                              org.clojure/clojure-contrib]]
-                 [compojure "0.6.5"]
-                 [hiccup "0.3.6"]
-                 [jline "0.9.94"]
-                 [hsqldb "1.8.0.10"]])
+                 [compojure "0.6.5"]])
